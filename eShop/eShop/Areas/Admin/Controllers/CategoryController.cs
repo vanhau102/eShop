82cd0d23eb1ac4ba81645dfde0e27b86b5af4661
vanhau <khaihau1002@gmail.com>
2022-08-29 20:09:49 +0700

    create and update category with viewmodel

---
+++ eShop/eShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using eShop.Areas.Admin.ViewModels.Category;
+using eShop.Database;
+using eShop.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.Areas.Admin.Controllers
+{
+	public class CategoryController : BaseController
+	{
+		public CategoryController(AppDbContext db) : base(db)
+		{
+		}
+
+		public IActionResult Index()
+		{
+			var query = _db.ProductCategories
+						.Select(c => new ListItemCategoryVM
+						{
+							Id = c.Id,
+							Name = c.Name,
+							CreatedAt = c.CreatedAt
+						})
+						.OrderByDescending(c => c.Id);
+			/*ViewBag.Sql = query.ToQueryString();*/
+			var data = query.ToList();
+			return View("Index", data);
+		}
+
+		public IActionResult Create() => View();
+
+		[HttpPost]
+		public IActionResult Create(AddOrUpdateCategoryVM categoryVM)
+		{
+			// xác thực dữ liệu
+			if (ModelState.IsValid == false)
+			{
+				return View(categoryVM);
+			}
+			// Lưu vào db
+			var category = new ProductCategory();
+			category.CreatedAt = DateTime.Now;
+			category.UpdatedAt = DateTime.Now;
+			category.Name = categoryVM.Name;
+			_db.ProductCategories.Add(category);
+			_db.SaveChanges();
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
---
+++ eShop/eShop/Program.cs
@@ -0,0 +1,41 @@
+using eShop.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// Kết nối db
+builder.Services.AddDbContext<AppDbContext>(opts =>
+{
+	var connectionString = builder.Configuration.GetConnectionString("Db");
+	opts.UseSqlServer(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapAreaControllerRoute(
+	name: "AdminArea",
+	areaName: "Admin",
+	pattern: "/Admin/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
